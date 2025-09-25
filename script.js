25154beff7da71144e6df2bee57da1249f9469eb
Maddie <232483035+maddiepdl@users.noreply.github.com>
2025-09-25 16:44:27 -0500

    script.js includes my song

---
+++ script.js
@@ -0,0 +1,17 @@
+   function refactoredRefrain() {
+const refrain = "You can stand under my umbrella,\nella, ella, eh, eh, eh";
+       return refrain;
+   }
+
+   function refactoredSong() {
+       console.log(refactoredRefrain());
+       console.log("Under my umbrella,");
+       console.log("ella, ella, eh, eh, eh.");
+       console.log(refactoredRefrain());
+   }
+
+   refactoredSong();
+
+
+
+
